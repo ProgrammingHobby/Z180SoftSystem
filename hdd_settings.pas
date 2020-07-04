9bf13ebdc7329d2315776e3f7b8a1073fa75856f
ProgrammingHobby <Uwe.Merker@KabelBW.de>
2020-07-04 10:55:42 +0200

    Speichern und Wiederherstellen der Einstellungen implementiert.

--- hdd_settings.pas
+++ hdd_settings.pas
@@ -4,9 +4,6 @@ unit Hdd_Settings;
 
 interface
 
-//uses
-//    Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
-
 uses
     Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls, EditBtn, SpinEx, Types;
 
@@ -37,6 +34,12 @@ type
         procedure FormShow(Sender: TObject);
         procedure HddGeometryChange(Sender: TObject);
     private
+        oldTracks: integer;
+        oldSectors: integer;
+        oldSectorBytes: string;
+        oldHeads: integer;
+        oldImageFile: string;
+        procedure calcHddSize;
 
     public
 
@@ -49,7 +52,7 @@ implementation
 
 {$R *.lfm}
 
-uses UscaleDPI, System_Settings;
+uses UscaleDPI, System_Settings, System_Hdc;
 
 { THddSettings }
 
@@ -57,11 +60,35 @@ uses UscaleDPI, System_Settings;
 procedure THddSettings.FormClose(Sender: TObject; var CloseAction: TCloseAction);
 begin
     SystemSettings.saveFormState(TForm(self));
+
+    if (oldHeads <> spineditHddHeads.Value) then begin
+        SystemSettings.WriteString('Hdd', 'Heads', IntToStr(spineditHddHeads.Value));
+        SystemHdc.setHddHeads(spineditHddHeads.Value);
+    end;
+    if (oldTracks <> spineditHddTracks.Value) then begin
+        SystemSettings.WriteString('Hdd', 'Tracks', IntToStr(spineditHddTracks.Value));
+        SystemHdc.setHddTracks(spineditHddTracks.Value);
+    end;
+    if (oldSectors <> spineditHddSectors.Value) then begin
+        SystemSettings.WriteString('Hdd', 'Sectors', IntToStr(spineditHddSectors.Value));
+        SystemHdc.setHddSectors(spineditHddSectors.Value);
+    end;
+    if (oldSectorBytes <> comboboxHddSectorBytes.Items[comboboxHddSectorBytes.ItemIndex]) then begin
+        SystemSettings.WriteString('Hdd', 'SectorBytes', comboboxHddSectorBytes.Items[comboboxHddSectorBytes.ItemIndex]);
+        SystemHdc.setHddSectorBytes(comboboxHddSectorBytes.Items[comboboxHddSectorBytes.ItemIndex].ToInteger);
+    end;
+    if (oldImageFile <> editHddImageFile.FileName) then begin
+        SystemSettings.WriteString('Hdd', 'ImageFile', editHddImageFile.FileName);
+        SystemHdc.setHddImage(editHddImageFile.FileName);
+    end;
+
     CloseAction := caFree;
 end;
 
 // --------------------------------------------------------------------------------
 procedure THddSettings.FormShow(Sender: TObject);
+var
+    ItemIndex: integer;
 begin
     SystemSettings.restoreFormState(TForm(self));
     ScaleDPI(self, 96);
@@ -70,9 +97,37 @@ begin
     Constraints.MaxWidth := Width;
     Constraints.MinHeight := Height;
     Constraints.MaxHeight := Height;
+
+    oldHeads := SystemSettings.ReadString('Hdd', 'Heads', '16').ToInteger;
+    spineditHddHeads.Value := oldHeads;
+
+    oldTracks := SystemSettings.ReadString('Hdd', 'Tracks', '246').ToInteger;
+    spineditHddTracks.Value := oldTracks;
+
+    oldSectors := SystemSettings.ReadString('Hdd', 'Sectors', '63').ToInteger;
+    spineditHddSectors.Value := oldSectors;
+
+    oldSectorBytes := SystemSettings.ReadString('Hdd', 'SectorBytes', '512');
+    ItemIndex := comboboxHddSectorBytes.Items.IndexOf(oldSectorBytes);
+    if (ItemIndex = -1) then begin
+        ItemIndex := 2;
+    end;
+    comboboxHddSectorBytes.ItemIndex := ItemIndex;
+
+    oldImageFile := SystemSettings.ReadString('Hdd', 'ImageFile', '');
+    editHddImageFile.FileName := oldImageFile;
+
+    calcHddSize;
 end;
 
+// --------------------------------------------------------------------------------
 procedure THddSettings.HddGeometryChange(Sender: TObject);
+begin
+    calcHddSize;
+end;
+
+// --------------------------------------------------------------------------------
+procedure THddSettings.calcHddSize;
 var
     tracks, sectors, bytes, heads, size: integer;
     sizeView: string;
