050b2797be5fafe14232020946940df1e46643e6
ProgrammingHobby <Uwe.Merker@KabelBW.de>
2021-01-23 15:02:36 +0100

    Einstellungsmöglichkeit für den Terminal-Typ hinzugefügt.

--- Sources/terminal_settings.pas
+++ Sources/terminal_settings.pas
@@ -15,18 +15,18 @@ type
         checkboxEnableLogging: TCheckBox;
         checkboxEnableLocalEcho: TCheckBox;
         checkboxEnableCrLf: TCheckBox;
-        groupboxTerminalColor: TGroupBox;
+        comboboxColorType: TComboBox;
+        groupboxColorType: TGroupBox;
         groupboxLogging: TGroupBox;
         groupboxLocalEcho: TGroupBox;
         groupboxCrLf: TGroupBox;
-        checkboxInverseTerminalScreen: TCheckBox;
         procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
         procedure FormShow(Sender: TObject);
     private
         oldEnableCrLf: boolean;
         oldEnableLocalEcho: boolean;
         oldEnableLogging: boolean;
-        oldInverseTerminal: boolean;
+        oldColorType: integer;
 
     public
 
@@ -57,11 +57,11 @@ begin
     end;
     if (checkboxEnableLogging.Checked <> oldEnableLogging) then begin
         SystemSettings.WriteBoolean('Terminal', 'Loggin', checkboxEnableLogging.Checked);
-        SystemTerminal.setTerminalLogging(checkboxEnableLogging.Checked);
+        SystemTerminal.setLogging(checkboxEnableLogging.Checked);
     end;
-    if(checkboxInverseTerminalScreen.Checked <> oldInverseTerminal) then begin
-        SystemSettings.WriteBoolean('Terminal', 'InverseScreen', checkboxInverseTerminalScreen.Checked);
-        SystemTerminal.setInverseScreen(checkboxInverseTerminalScreen.Checked);
+    if (comboboxColorType.ItemIndex <> oldColorType) then begin
+        SystemSettings.WriteInteger('Terminal', 'ColorType', comboboxColorType.ItemIndex);
+        SystemTerminal.setColorType(comboboxColorType.ItemIndex);
     end;
     CloseAction := caFree;
 end;
@@ -82,8 +82,8 @@ begin
     checkboxEnableLocalEcho.Checked := oldEnableLocalEcho;
     oldEnableLogging := SystemSettings.ReadBoolean('Terminal', 'Loggin', False);
     checkboxEnableLogging.Checked := oldEnableLogging;
-    oldInverseTerminal := SystemSettings.ReadBoolean('Terminal', 'InverseScreen', False);
-    checkboxInverseTerminalScreen.Checked:=oldInverseTerminal;
+    oldColorType := SystemSettings.ReadInteger('Terminal', 'ColorType', 0);
+    comboboxColorType.ItemIndex := oldColorType;
 end;
 
 // --------------------------------------------------------------------------------
